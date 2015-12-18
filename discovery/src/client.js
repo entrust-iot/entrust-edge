49a4671db0cd879d61c371c89297977c674056de
Xavier LaRue <xlarue@macadamian.com>
2015-12-18 13:11:11 -0500

    Test to get ip from the OS

--- discovery/src/client.js
+++ discovery/src/client.js
@@ -2,9 +2,7 @@ var dgram = require('dgram');
 var socket = dgram.createSocket('udp4');
 var broadcastPort = 5555;
 
-socket.bind(broadcastPort, function() {
-  socket.setBroadcast(true);
-});
+socket.bind(broadcastPort);
 
 socket.on("message", function ( data, rinfo ) {
 	console.log("Message received from ", rinfo.address, " : ", data.toString());
